21413ea49f44bc982c55a5864b6478793d030e38
Dan Greaves <dan@dangreaves.com>
2024-09-18 15:28:22 +1000

    test: structure tests to accommodate other response types

--- src/unwrap.test.ts
+++ src/unwrap.test.ts
@@ -1,114 +1,120 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 
 import type { GraphQLResponse } from "./types";
 
 import { unwrap } from "./unwrap";
 
-it("returns only given operation from data", async () => {
-  const unwrapped = await unwrap(MOCK_RESPONSE_SUCCESS, "productCreate");
+/**
+ * Some API clients, like the Admin API client returned from the @shopify/shopify-app-remix
+ * package return fetch responses rather than just the data.
+ *
+ * These types of responses need to be unwrapped in a different way to other clients which
+ * return just the data object.
+ *
+ * @see https://shopify.dev/docs/api/shopify-app-remix/v3/guide-admin#graphql-api
+ */
+describe("fetch response", () => {
+  it("returns only given operation from data", async () => {
+    const unwrapped = await unwrap(
+      mockFetchResponse(MOCK_RESPONSE_SUCCESS),
+      "productCreate",
+    );
 
-  expect(unwrapped).toEqual({
+    expect(unwrapped).toEqual({
+      product: {
+        title: "Cotton T-Shirt",
+        id: "gid://shopify/Product/5070746714248",
+      },
+    });
+  });
+
+  it("returns only given resource from data", async () => {
+    const unwrapped = await unwrap(
+      mockFetchResponse(MOCK_RESPONSE_SUCCESS),
+      "productCreate",
+      "product",
+    );
+
+    expect(unwrapped).toEqual({
+      title: "Cotton T-Shirt",
+      id: "gid://shopify/Product/5070746714248",
+    });
+  });
+
+  it("throws exception when no data returned in response", async () => {
+    await expect(() =>
+      unwrap(mockFetchResponse(MOCK_RESPONSE_NO_DATA), "productCreate"),
+    ).rejects.toThrowError("No data returned in shopify response.");
+  });
+
+  it("throws exception when operation does not exist in response", async () => {
+    await expect(() =>
+      unwrap(mockFetchResponse(MOCK_RESPONSE_NO_OPERATION), "productCreate"),
+    ).rejects.toThrowError(
+      `Operation "productCreate" does not exist in shopify response.`,
+    );
+  });
+
+  it("throws exception when resource does not exist in response", async () => {
+    await expect(() =>
+      unwrap(
+        mockFetchResponse(MOCK_RESPONSE_NO_RESOURCE),
+        "productCreate",
+        "product",
+      ),
+    ).rejects.toThrowError(
+      `Resource "product" does not exist in shopify response.`,
+    );
+  });
+
+  it("throws exception when user errors returned in response", async () => {
+    await expect(() =>
+      unwrap(mockFetchResponse(MOCK_RESPONSE_USER_ERRORS), "productCreate"),
+    ).rejects.toThrowError("User errors returned in shopify response.");
+  });
+});
+
+/**
+ * With the given data, make a mock fetch response.
+ * This is how the admin API client returns data to the user.
+ */
+function mockFetchResponse<T>(data: T): GraphQLResponse<T> {
+  return {
+    json: () => Promise.resolve({ data }),
+  } as GraphQLResponse<T>;
+}
+
+/**
+ * Response type for the productCreate mutation.
+ */
+type MockProductCreate = {
+  productCreate?: {
+    product?: { id: string; title: string } | null;
+    userErrors?: { field: string; message: string }[];
+  } | null;
+} | null;
+
+const MOCK_RESPONSE_SUCCESS: MockProductCreate = {
+  productCreate: {
     product: {
       title: "Cotton T-Shirt",
       id: "gid://shopify/Product/5070746714248",
     },
-  });
-});
+    userErrors: [],
+  },
+};
 
-it("returns only given resource from data", async () => {
-  const unwrapped = await unwrap(
-    MOCK_RESPONSE_SUCCESS,
-    "productCreate",
-    "product",
-  );
+const MOCK_RESPONSE_NO_DATA: MockProductCreate = null;
 
-  expect(unwrapped).toEqual({
-    title: "Cotton T-Shirt",
-    id: "gid://shopify/Product/5070746714248",
-  });
-});
+const MOCK_RESPONSE_USER_ERRORS: MockProductCreate = {
+  productCreate: {
+    product: null,
+    userErrors: [{ field: "handle", message: "Handle already exists" }],
+  },
+};
 
-it("throws exception when no data returned in response", async () => {
-  await expect(() =>
-    unwrap(MOCK_RESPONSE_NO_DATA, "productCreate"),
-  ).rejects.toThrowError("No data returned in shopify response.");
-});
+const MOCK_RESPONSE_NO_OPERATION: MockProductCreate = {};
 
-it("throws exception when operation does not exist in response", async () => {
-  await expect(() =>
-    unwrap(MOCK_RESPONSE_NO_OPERATION, "productCreate"),
-  ).rejects.toThrowError(
-    `Operation "productCreate" does not exist in shopify response.`,
-  );
-});
-
-it("throws exception when resource does not exist in response", async () => {
-  await expect(() =>
-    unwrap(MOCK_RESPONSE_NO_RESOURCE, "productCreate", "product"),
-  ).rejects.toThrowError(
-    `Resource "product" does not exist in shopify response.`,
-  );
-});
-
-it("throws exception when user errors returned in response", async () => {
-  await expect(() =>
-    unwrap(MOCK_RESPONSE_USER_ERRORS, "productCreate"),
-  ).rejects.toThrowError("User errors returned in shopify response.");
-});
-
-type MockProductCreate = {
-  productCreate?: {
-    product?: { id: string; title: string } | null;
-    userErrors: { field: string; message: string }[];
-  } | null;
-} | null;
-
-const MOCK_RESPONSE_SUCCESS = {
-  json: () =>
-    Promise.resolve({
-      data: {
-        productCreate: {
-          product: {
-            title: "Cotton T-Shirt",
-            id: "gid://shopify/Product/5070746714248",
-          },
-          userErrors: [],
-        } as MockProductCreate,
-      },
-    }),
-} as GraphQLResponse<MockProductCreate>;
-
-const MOCK_RESPONSE_NO_DATA = {
-  json: () =>
-    Promise.resolve({
-      data: null,
-    }),
-} as GraphQLResponse<MockProductCreate>;
-
-const MOCK_RESPONSE_USER_ERRORS = {
-  json: () =>
-    Promise.resolve({
-      data: {
-        productCreate: {
-          product: null,
-          userErrors: [{ field: "handle", message: "Handle already exists" }],
-        } as MockProductCreate,
-      },
-    }),
-} as GraphQLResponse<MockProductCreate>;
-
-const MOCK_RESPONSE_NO_OPERATION = {
-  json: () =>
-    Promise.resolve({
-      data: {},
-    }),
-} as GraphQLResponse<MockProductCreate>;
-
-const MOCK_RESPONSE_NO_RESOURCE = {
-  json: () =>
-    Promise.resolve({
-      data: {
-        productCreate: {},
-      },
-    }),
-} as GraphQLResponse<MockProductCreate>;
+const MOCK_RESPONSE_NO_RESOURCE: MockProductCreate = {
+  productCreate: {},
+};
